b7a1cba99d66474b4c9873c0fbb1820a12ebdaed
MCSH <mcshemail@gmail.com>
2017-07-23 22:35:32 +0430

    bug fix

--- index.js
+++ index.js
@@ -232,5 +232,5 @@ function clone(obj){
     return JSON.parse(JSON.stringify(obj));
 }
 
-manager.Store = require('lib/store.js');
+manager.Store = require('./lib/store.js');
 module.exports = manager;
